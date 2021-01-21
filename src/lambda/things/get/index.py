3cd100e5ba6b9680d6ba7c7e2324d192bc575539
Kevin Shaffer-Morrison <shafkevi@amazon.com>
2021-01-21 14:06:45 -0500

    update endpoint to be able to get one thing at a time

--- src/lambda/things/get/index.py
+++ src/lambda/things/get/index.py
@@ -11,20 +11,34 @@ def handler(event, context):
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(TABLE_NAME)
     user_id = event["requestContext"]["authorizer"]["jwt"]["claims"]["sub"]
+    if "pathParameters" in event:
+        thing_id = event.get("pathParameters").get("id")
+    else:
+        thing_id = None
     try:
-        query = event.get("queryStringParameters", {})
-        if query is not None:
-            limit = query.get("limit", 60)
-        else:
-            limit = 60
-        sort_key = 'thing'
-        response = table.query(
-            ScanIndexForward=False,
-            Limit=limit,
-            KeyConditionExpression=Key('partitionKey').eq(user_id) & Key('sortKey').begins_with(sort_key)
-        )
-        print(response["Items"])
-        return response["Items"]
+        if thing_id:
+            response = table.get_item(
+                TableName=TABLE_NAME,
+                Key={
+                "partitionKey": user_id,
+                "sortKey": "thing_{}".format(thing_id)
+            })
+            print(response["Item"])
+            return response["Item"]
+        else: 
+            query = event.get("queryStringParameters", {})
+            if query is not None:
+                limit = query.get("limit", 60)
+            else:
+                limit = 60
+            sort_key = 'thing'
+            response = table.query(
+                ScanIndexForward=False,
+                Limit=limit,
+                KeyConditionExpression=Key('partitionKey').eq(user_id) & Key('sortKey').begins_with(sort_key)
+            )
+            print(response["Items"])
+            return response["Items"]
     except Exception as e:
         print(e)
         return {"message": "error!"}
